7bd1355b8c8a26d220c12adb673953eda80daf8a
AKats1995 <118009276+AKats1995@users.noreply.github.com>
2025-11-17 12:44:39 +0200

    Implement interactive calculator functionality
    Added a main method to interactively use the calculator.

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,53 @@
-public class Calculator {
+public class Calculator {public static void main (String [] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to use Calculator Y/N ");
+        String answer = scanner.nextLine();
+        while (answer.equals("Y")) {
+        System.out.println("Give me the First number: ");
+        int number1 = scanner.nextInt();
+        System.out.println("Give me the Second number: ");
+        int number2 = scanner.nextInt();
+        System.out.println("What do you want to do : +, −, *  or ÷) ");
+        scanner.nextLine(); 
+        String mark = scanner.nextLine();
+        int total;
+        switch (mark) {
+            case "+":
+            total = number1 + number2;
+            System.out.println(total);
+            break;
+            case "-":
+            total = number1 - number2;
+            System.out.println(total);
+            break;
+            case "*":
+            total = number1 * number2;
+            System.out.println(total);
+            break;
+            case "/":
+            if (number2 != 0  ) {
+                total = number1 / number2;
+                 System.out.println(total);}
+            else{
+                System.out.println("Dont Give 0");
+            }
+            break;
+        }
+        System.out.println("Do you want to continue the use of the Calculator Y/N ");
+        answer = scanner.nextLine();
+
+                
+       }
+
+
+            
+
+
+    }
+
+}
+
+  
 
 }
